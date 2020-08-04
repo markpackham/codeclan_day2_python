9d7c73582e11dbefcf6764c91265c2813ff62a72
mark <markpackham1@gmail.com>
2020-08-04 11:11:24 +0100

    modify agent talk code

--- miniLab/agent.py
+++ miniLab/agent.py
@@ -5,8 +5,13 @@ class Agent(Person):
     def __init__(self, first_name, last_name):
         Person.__init__(self, first_name, last_name)
 
+    # This will get overidden by the one below
     def talk(self):
         return "The names Bond, James Bond"
+
+    # The bottom one takes overrides the top
+    def talk(self):
+        return f"The names {self.last_name}, {self.first_name} {self.last_name}"
 
 
 new_agent = Agent("Agent", "X")
