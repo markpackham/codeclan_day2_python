7d291dde1efddb97e9c1536e9918e0c262b78581
mark <markpackham1@gmail.com>
2020-08-04 11:41:32 +0100

    Add Iterator example

---
+++ iterators.py
@@ -0,0 +1,32 @@
+# # does initial setup and returns and iterator object(calls__next()___)
+# __iter__()
+
+# # return the next value in the sequence
+# __next__()
+
+# # Any object with an __iter__ and __next__ is iterable
+
+import random
+
+# pick 6 random numbers, make sure there are no duplicates
+class LotterMachine:
+    def __iter__(self):
+        self.numbers = []
+        return self
+
+    def __next__(self):
+        # We only want 6 numbers
+        if len(self.numbers) == 6:
+            raise StopIteration
+        else:
+            number = random.randint(1, 49)
+            # make sure we don't get duplicates
+            if number not in self.numbers:
+                self.numbers.append(number)
+                return number
+
+
+machine = LotterMachine()
+# it has an iter and next so we can loop through it
+for number in machine:
+    print(number)
