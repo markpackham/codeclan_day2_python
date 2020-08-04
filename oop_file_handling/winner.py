42b4e3e7782c96913c675914b5375c7598040c03
mark <markpackham1@gmail.com>
2020-08-04 15:21:20 +0100

    write to winner file

--- oop_file_handling/winner.py
+++ oop_file_handling/winner.py
@@ -30,19 +30,30 @@ with open("oscars.csv", "r") as csvfile:
         current_winner = Winner(*row)
         winners.append(current_winner)
     
-for winner in winners:
-    print(f"{winner.name} won the Oscar for {winner.movie} in {winner.year} at {winner.age}")
+# for winner in winners:
+#     print(f"{winner.name} won the Oscar for {winner.movie} in {winner.year} at {winner.age}")
 
-with open("oscars.csv") as csvfile:
-    reader = csv.reader(csvfile)
-    # give me 1 number for row in reader
-    count = sum(1 for row in reader)
-    #print("Number of rows: " + str(count))
+# with open("oscars.csv") as csvfile:
+#     reader = csv.reader(csvfile)
+#     # give me 1 number for row in reader
+#     count = sum(1 for row in reader)
+#     #print("Number of rows: " + str(count))
 
-# a for append (add stuff)
-with open("oscars.csv","a") as csvfile:
-    # quoting is a keyword
+# # a for append (add stuff)
+# with open("oscars.csv","a") as csvfile:
+#     # quoting is a keyword
+#     writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+#     winner = Winner(count, 2020, 50, "Renne Zellweger", "Judy")
+#     # writer.writerow([winner.index, winner.year, winner.age, winner.name, winner.movie])
+#     writer.writerow(winner.values())
+
+# always read "r" before write, "w"
+with open("oscars.csv", "w") as csvfile:
     writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-    winner = Winner(count, 2020, 50, "Renne Zellweger", "Judy")
-    # writer.writerow([winner.index, winner.year, winner.age, winner.name, winner.movie])
-    writer.writerow(winner.values())
+
+    writer.writerow(["Index", "Year", "Age", "Name", "Movie"])
+
+    for winner in winners:
+        # make winners a bit older
+        winner.age += 1
+        writer.writerow(winner.values())
