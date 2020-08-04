d4a6a7049d3df8f28f9a550801c2be8c932a7364
mark <markpackham1@gmail.com>
2020-08-04 16:43:40 +0100

    make sure we add Jenny before we read her

--- file_handling_lab/employees.py
+++ file_handling_lab/employees.py
@@ -4,14 +4,19 @@ class Employee:
     def __init__(self, first_name, last_name, hourly_rate, hours_worked, amount_due):
         self.first_name = first_name
         self.last_name = last_name
-        self.hourly_rate = hourly_rate
-        self.hours_worked = hours_worked
-        self.amount_due = amount_due
+        self.hourly_rate = float(hourly_rate)
+        self.hours_worked = float(hours_worked)
+        self.amount_due = float(amount_due)
     
     def values(self):
         return [self.first_name, self.last_name, self.hourly_rate, self.hours_worked, self.amount_due]
 
 employees = []
+
+with open("employees.csv", "a", newline='') as csvfile:
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    employee = Employee("Jenny","Jones",12.50,40,0)
+    writer.writerow(employee.values())
 
 with open("employees.csv", "r") as csvfile:
     next(csvfile)
@@ -20,11 +25,6 @@ with open("employees.csv", "r") as csvfile:
     for row in reader:
         current_employee = Employee(*row)
         employees.append(current_employee)
-
-with open("employees.csv", "a", newline='') as csvfile:
-    writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-    employee = Employee("Jenny","Jones","12.50","40","0")
-    writer.writerow(employee.values())
 
 with open("employees.csv", "w") as csvfile:
     writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
