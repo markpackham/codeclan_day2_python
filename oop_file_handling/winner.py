a08e245a66329937455d8c71899a185329857dde
mark <markpackham1@gmail.com>
2020-08-04 14:47:33 +0100

    Output winner data

--- oop_file_handling/winner.py
+++ oop_file_handling/winner.py
@@ -18,4 +18,9 @@ with open("oscars.csv", "r") as csvfile:
     reader = csv.reader(csvfile, skipinitialspace=True)
 
     for row in reader:
-        print(row)
+        #print(row)
+        current_winner = Winner(row[0], row[1],row[2],row[3],row[4])
+        winners.append(current_winner)
+    
+for winner in winners:
+    print(f"{winner.name} won the Oscar for {winner.movie} in {winner.year} at {winner.age}")
