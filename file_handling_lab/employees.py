cea689aa2473e399f28c6cec16ce0bc8f29d7461
mark <markpackham1@gmail.com>
2020-08-04 16:32:09 +0100

    start with cleaner .csv

--- file_handling_lab/employees.py
+++ file_handling_lab/employees.py
@@ -4,9 +4,9 @@ class Employee:
     def __init__(self, first_name, last_name, hourly_rate, hours_worked, amount_due):
         self.first_name = first_name
         self.last_name = last_name
-        self.hourly_rate = float(hourly_rate)
-        self.hours_worked = int(hours_worked)
-        self.amount_due = float(amount_due)
+        self.hourly_rate = hourly_rate
+        self.hours_worked = hours_worked
+        self.amount_due = amount_due
     
     def values(self):
         return [self.first_name, self.last_name, self.hourly_rate, self.hours_worked, self.amount_due]
@@ -15,7 +15,7 @@ employees = []
 
 with open("employees.csv", "a", newline='') as csvfile:
     writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-    employee = Employee("Jenny","Jones",12.50,40,0)
+    employee = Employee("Jenny","Jones","12.50","40","0")
     writer.writerow(employee.values())
 
 
@@ -35,6 +35,5 @@ with open("employees.csv", "w") as csvfile:
     writer.writerow(["first_name", "last_name", "hourly_rate", "hours_worked", "amount_due"])
 
     for employee in employees:
-        amount_due = float(employee.hourly_rate) * int(employee.hours_worked)
-        new_employee = Employee(employee.first_name, employee.last_name, employee.hourly_rate, employee.hours_worked, amount_due)
-        writer.writerow(new_employee)
+        employee.amount_due = "1000"
+        writer.writerow(employee.values())
